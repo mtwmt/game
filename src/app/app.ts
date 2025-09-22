1fd3bca9bae4db55eb72c0c5de4c69f4cfdd6c22
Mandy <mtwmt@hotmail.com>
2025-09-23 00:53:32 +0800

    feat: Add mobile touch controls and UI enhancements

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Guideline } from './guideline/guideline';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, Guideline],
+  imports: [RouterOutlet, Guideline],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('game');
+
+  year = new Date().getFullYear();
 }
